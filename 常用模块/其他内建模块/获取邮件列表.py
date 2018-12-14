84936855fd9a28a5fc3af2c687e585e2826b826a
beijing_web <17710890916@163.com>
2018-12-14 21:44:16 +0800

    进程和正则

--- 常用模块/其他内建模块/获取邮件列表.py
+++ 常用模块/其他内建模块/获取邮件列表.py
@@ -1,7 +1,46 @@
-import imaplib,datetime
-with imaplib.IMAP4_SSL('imaphm.qiye.163.com',993) as m:
-    m.login('reg.server-005@mexmarkets.com','19861119Kk')
-    print(m.list())
-    m.select('Sent')
-    type,data = m.search(None,'(SENTON "13-Dec-2018")')
-    print(data[0].decode('utf-8'))
+import imaplib,datetime,pymysql
+class mail_163:
+    def __init__(self,accout):
+        self.account = accout
+
+
+def mailcount(num):
+    """获取邮箱的当天的发件数量"""
+    with imaplib.IMAP4_SSL('imaphm.qiye.163.com',993) as m:
+        mailaccount = ("reg.server-00%s@mexmarkets.com" % num)
+        m.login(mailaccount,'19861119Kk' )
+        m.select('Sent')
+        # 获取当天日期并转换为邮件日期格式
+        C_date = datetime.datetime.strftime(datetime.date.today(),'%d-%B-%Y')
+        type,data = m.search(None,'(SENTON %s)' % C_date)
+        a = data[0].decode('utf-8')
+        return len(a.split(" "))
+#print(mailcount('1'))
+DB_name = 'countmail'
+DB_host = '39.105.13.50'
+DB_pass = '20140916'
+DB_user = 'root'
+mail_server = 'smtphm.qiye.163.com'
+mail_user = 'reg.server-005@mexmarkets.com'
+mail_pass = '19861119Kk'
+mail_port = 994
+mail_protocol = 'ssl'
+def update_mail():
+    db = pymysql.connect(DB_host, DB_user, DB_pass, DB_name)
+    cursor = db.cursor()
+    try:
+        cursor.execute("update mails set server='%s',user='%s',pass='%s',port=%d,sec_protocol='%s'" % (mail_server,mail_user,mail_pass,mail_port,mail_protocol))
+        db.commit()
+    except:
+        db.rollback()
+    finally:
+        db.close()
+update_mail()
+def get_cu_mail(arg):
+    dab = pymysql.connect(DB_host, DB_user, DB_pass, DB_name)
+    cur =dab.cursor()
+    cur.execute("select %s from mails" % arg)
+    result = cur.fetchone()
+    dab.close()
+    return  result
+get_cu_mail('user')
